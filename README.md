37bf5f96f4bc7c7bd00d208a81ba9b275966f48a
hashelq <suramaru0@gmail.com>
2022-05-29 15:17:28 +1000

    Initialize

---
+++ README.md
@@ -0,0 +1,52 @@
+# hashdistinct
+It is simple utility for deletion duplications.
+
+## Installation
+```
+cargo install hashdistinct
+```
+
+## Usage
+It is simple as ls. Just look at the usage:
+
+```
+Distinct Hash 0.4.0
+Starfear https://github.com/starfear
+Utility for deletion duplications with same hash.
+
+USAGE:
+    hashdistinct [FLAGS] [OPTIONS] <targets>...
+
+FLAGS:
+    -h, --help       Prints help information
+    -s, --silent     silent mode
+    -V, --version    Prints version information
+
+OPTIONS:
+    -a, --algorithm <algorithm>    hash algorithm. Supported algorithms: [SHA256, SHA384, SHA512, SHA512_256]
+
+ARGS:
+    <targets>...    targets
+```
+
+## Usage examples
+```
+# raw
+hashdistinct foo.webm bar.webm too.webm
+
+# for sh, bash zsh
+hashdistinct webms/*
+
+# specify algorithm (SHA256, SHA384, SHA512, SHA512_256)
+hashdistinct webms/* -a SHA512_256
+
+# if you prefer silent mode
+hashdistinct webms/* -a SHA512_256 --silent
+```
+
+## Contribute
+Seems like this software doesn't have to be improved, but I don't mind to add support for md5 and other non-ring algorithms.
+Pull requests and issues are welcome.
+
+## License
+Under GNU/GPL3
---
+++ src/main.rs
@@ -0,0 +1,132 @@
+extern crate clap;
+
+use {
+    colored::{Colorize},
+
+    std::{
+        collections::{BTreeMap},
+        fs::{File},
+        io::{Result, BufReader, Read}
+    },
+
+    clap::{Arg, App},
+
+    ring::{digest::{Context, Digest, SHA256, SHA384, SHA512, SHA512_256}},
+};
+
+fn status(silent: bool, msg: &str) {
+    if silent == false {
+        println!("{} {}", "STATUS".green(), msg);
+    }
+}
+
+fn info(silent: bool, msg: &str) {
+    if silent == false {
+        println!("{} {}", "INFO".blue(), msg);
+    }
+}
+
+fn error(silent: bool, msg: &str) {
+    if silent == false {
+        println!("{} {}", "ERROR".red(), msg);
+    }
+}
+
+fn main() -> Result<()> {
+    let matches = App::new("Distinct Hash")
+                    .version("0.4.0")
+                    .author("Hashelq https://github.com/hashelq")
+                    .about("Utility for deletion duplications with same hash.")
+                    .arg(Arg::with_name("silent")
+                        .help("silent mode")
+                        .long("silent")
+                        .short("s"))
+                    .arg(Arg::with_name("targets")
+                        .help("targets")
+                        .required(true)
+                        .index(1)
+                        .multiple(true)
+                    )
+                    .arg(Arg::with_name("algorithm")
+                        .help("hash algorithm. Supported algorithms: [SHA256, SHA384, SHA512, SHA512_256]")
+                        .short("a")
+                        .long("algorithm")
+                        .takes_value(true))
+                    .get_matches();
+
+    let silent = matches.is_present("silent");
+    let alg = matches.value_of("algorithm").unwrap_or("SHA256").to_uppercase();
+    let algorithm = match alg.as_str() {
+        "SHA256"     => &SHA256,
+        "SHA384"     => &SHA384,
+        "SHA512"     => &SHA512,
+        "SHA512_256" => &SHA512_256,
+
+        _ => {
+            eprintln!("{} Hash algorithm '{}' is not supported.\nSupported algorithms: [SHA256, SHA384, SHA512, SHA512_256]", "ERROR".red(), alg.red());
+            std::process::exit(1);
+        }
+    };
+
+    // collect meta data
+    let targets = matches.values_of("targets").unwrap();
+    status(silent, "Collecting metadata (size) . . .");
+
+    let mut sizes: BTreeMap<u64, Vec<&str>> = BTreeMap::new();
+    for target in targets {
+        let meta = std::fs::metadata(target)?;
+
+        match sizes.get_mut(&meta.len()) {
+            Some(v) => v.push(target),
+            None    => { sizes.insert(meta.len(), vec![target]); }
+        };
+    }
+
+    // calculate hashes
+    status(silent, "Calculating hashes . . .");
+
+    let mut to_remove = Vec::new();
+    
+    for (size, mut targets) in sizes {
+        if targets.len() == 1 { continue; }
+
+        info(silent, &format!("Size: {}, els: {}", size, targets.len()));
+        let hash = sum(BufReader::new(File::open(targets.remove(0))?), algorithm)?.as_ref().to_vec();
+    
+        for target in targets {
+            if hash == sum(BufReader::new(File::open(target)?), algorithm)?.as_ref() {
+                info(silent, &format!("Found duplicate: {}", target));
+                to_remove.push(target);
+            }
+        }
+    }
+
+    info(silent, &format!("Files to delete: {}", to_remove.len().to_string().red()));
+
+    for target in to_remove {
+        info(silent, &format!("Delete file {:?}", target));
+
+        match std::fs::remove_file(target) {
+            Ok(_)  => (),
+            Err(e) => error(silent, &format!("Failed to delete file {:?}: '{}'", target, e))
+        };
+    }
+
+    status(silent, "All duplications were deleted");
+    Ok(())
+}
+
+fn sum<R: Read>(mut reader: R, algorithm: &'static ring::digest::Algorithm) -> Result<Digest> {
+    let mut context = Context::new(algorithm);
+    let mut buffer = [0; 1024];
+
+    loop {
+        let count = reader.read(&mut buffer)?;
+        if count == 0 {
+            break;
+        }
+        context.update(&buffer[..count]);
+    }
+
+    Ok(context.finish())
+}
